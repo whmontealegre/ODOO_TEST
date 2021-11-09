a1c65cc181b52a2f69a606c8f316eb6e4232a3fd
whmontealegre <walter.montealegre@roottco.com>
2021-11-09 20:19:31 +0000

    Update 9-11

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,33 @@
+#-*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """academy app to manage Trainig""",
+    
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        -Sessions
+        -Attendees
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'date': [
+        
+        
+    ],
+    
+    'demo': [
+       'demo/academy_demo.xml',
+        
+    ],
+}
--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -3,6 +3,14 @@
 from odoo import models, fields, api
 
 class Course(models.Model):
-
+    
     _name='academy.course'
-    _description='Course Info'
+    _description='Course Info test 1'
+    
+    name = fields.Char(string = 'Title', required=True)
+    description = fields.Text(string='Description')
+    level = fields.Selection(string='Level', selection=[('beginner', 'Beginner'), 
+                                                       ('intermediate', 'Intermediate'),
+                                                      ('advanced', 'Advanced')],
+                           copy=False)
+    active = fields.Boolean(string='Active', default=True)
